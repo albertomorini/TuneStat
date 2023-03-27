170d0b631030d9b28a6414c48319f3c7bcba86f0
albertomorini <99morini@gmail.com>
2023-03-27 20:03:34 +0200

    added a draft of visualization of stats

--- TuneStat.py
+++ TuneStat.py
@@ -5,38 +5,59 @@ from tinytag import TinyTag
 from mutagen.mp4 import MP4
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, USLT, TCOM, TCON, TDRC
+import datetime
 
 
 
-
-def getInfoSong(pathSong, artist):
+def getInfoArtists(pathSong, artistsCollection):
 	try:
 		metadata = TinyTag.get(pathSong)
 
-		if(not metadata.artist in artist.keys()):
-			artist[metadata.artist] = {}
+		if(not metadata.artist in artistsCollection.keys()):
+			artistsCollection[metadata.artist] = {}
 
-		if(not metadata.album in artist[metadata.artist].keys()):
-			#print(metadata)
-			artist[metadata.artist][metadata.album]={
+		if(not metadata.album in artistsCollection[metadata.artist].keys()):
+			artistsCollection[metadata.artist][metadata.album]={
 				"genre":metadata.genre,
-				"date":metadata.year,
+				"date":metadata.year[0:4] if metadata.year!=None else None,
 				"nrOfSongs": 1 ## starts from 1 cause we're scanning songs (so the first one creates the entry and counts itself)
 			}
 		else:
-			artist[metadata.artist][metadata.album]["nrOfSongs"] += 1
+			artistsCollection[metadata.artist][metadata.album]["nrOfSongs"] += 1
 
-		return artist
+		return artistsCollection
 	except Exception as e:
 		print(e)
 		return None
+
+
+def showStats(artistsCollection):
+	for i in artistsCollection.keys():
+		print(i)
+		print("Nr of albums:",len(artistsCollection[i].keys()))
+		for j in artistsCollection[i]:
+			print(artistsCollection[i][j])
+'''
+Future
+Nr of albums: 10
+{'genre': 'Hip-Hop/Rap', 'date': '2012', 'nrOfSongs': 16}
+{'genre': 'Hip Hop, Trap', 'date': '2015', 'nrOfSongs': 8}
+{'genre': 'Hip-Hop / Rap', 'date': '2016', 'nrOfSongs': 1}
+{'genre': 'Hip-Hop', 'date': '2017', 'nrOfSongs': 17}
+{'genre': 'Hip-Hop/Rap', 'date': None, 'nrOfSongs': 18}
+{'genre': 'Hip-Hop / Rap', 'date': '2020', 'nrOfSongs': 21}
+{'genre': 'Hip-Hop / Rap', 'date': '2016', 'nrOfSongs': 1}
+{'genre': 'Hip-Hop/Rap', 'date': '2022', 'nrOfSongs': 22}
+{'genre': 'Hip-Hop / Rap', 'date': '2019', 'nrOfSongs': 1}
+{'genre': 'Hip Hop, Trap', 'date': '2017', 'nrOfSongs': 17}
+'''
 
 
 def main(path):
 	dictSongs = {
 		"numSongs":0,
 	}
-	artists ={
+	artistsCollection ={
 
 	}
 	for root, directories, files in os.walk(path, topdown=True):
@@ -44,10 +65,10 @@ def main(path):
 				pathTmp=str(os.path.join(root, name))	
 				if(pathTmp.endswith(".m4a") or pathTmp.endswith(".mp3")):
 					dictSongs["numSongs"]+=1
-					getInfoSong(pathTmp, artists)
+					getInfoArtists(pathTmp, artistsCollection)
 
 
-	print(artists)
+	showStats(artistsCollection)
 
 
 
